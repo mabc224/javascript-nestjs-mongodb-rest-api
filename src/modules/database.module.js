f6fe01c499ff6bca7abf11cb95b169e7a419897f
mabc224 <mabc224@gmail.com>
2021-12-16 18:51:19 +0500

    Fix database module to use new url parser

--- src/modules/database.module.js
+++ src/modules/database.module.js
@@ -2,6 +2,6 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [MongooseModule.forRoot(process.env.DB_URI)],
+  imports: [MongooseModule.forRoot(process.env.DB_URI, { useNewUrlParser: true })],
 })
 export default class DatabaseModule {}
