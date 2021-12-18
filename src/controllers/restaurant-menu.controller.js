c93f25a363515aefc1513b16d88c69d06678daea
mabc224 <mabc224@gmail.com>
2021-12-18 13:54:48 +0500

    Add controllers with functionality

--- src/controllers/restaurant-menu.controller.js
+++ src/controllers/restaurant-menu.controller.js
@@ -1,15 +1,202 @@
 import {
-  Controller,
-  Logger,
+  Bind, Body,
+  Controller, Delete, Dependencies, Get, HttpCode, HttpException, HttpStatus,
+  Logger, Param, Post, Put, Query,
 } from '@nestjs/common';
+import { RestaurantMenuService, RestaurantService } from '../services';
 
 @Controller({
-  path: 'restaurants/:id',
+  path: 'restaurants/:restaurantId/menus',
   version: ['0.1'],
 })
 
+@Dependencies(RestaurantMenuService, RestaurantService)
 export default class RestaurantMenuController {
-  constructor() {
+  constructor(restaurantMenuService, restaurantService) {
+    this.restaurantMenuService = restaurantMenuService;
+    this.restaurantService = restaurantService;
+
     this.logger = new Logger('restaurant-menu.controller');
+  }
+
+  @Get('/')
+  @Bind(Param(), Query())
+  async listRestaurantMenus(params, queryParams) {
+    const { restaurantId } = params;
+    const {
+      sortBy = 'createdAt',
+      order = 'asc',
+    } = queryParams;
+    let { page, perPage } = queryParams;
+
+    page = page ? parseInt(page, 10) : 1;
+    perPage = perPage ? parseInt(perPage, 10) : 10;
+
+    const ordering = order === 'asc' ? 1 : -1;
+
+    const query = { restaurantId };
+    const skip = perPage * (page - 1);
+    const limit = perPage;
+    const sort = { [sortBy]: ordering };
+
+    const [totalCount, restaurants] = await Promise.all([
+      this.restaurantMenuService.countRestaurantMenus(query),
+      this.restaurantMenuService.listRestaurantMenus(query, skip, limit, sort),
+    ]);
+
+    const totalPages = parseInt(Math.ceil(totalCount / perPage), 10);
+    if (totalCount <= perPage) {
+      page = 1;
+    } else if (page > totalPages) {
+      page = totalPages;
+    }
+
+    return {
+      menus: restaurants.map((restaurant) => this.getApiRestaurantMenu(restaurant)),
+      page,
+      perPage,
+      totalCount,
+    };
+  }
+
+  @Post('/')
+  @Bind(Param(), Body())
+  async createRestaurantMenu(params, body) {
+    const { restaurantId } = params;
+    const {
+      name, picture, price, category,
+    } = body;
+
+    const query = {
+      _id: restaurantId,
+    };
+    const restaurantExist = await this.restaurantService.getRestaurant(query);
+
+    if (!restaurantExist) {
+      throw new HttpException('Restaurant not found', HttpStatus.NOT_FOUND);
+    }
+
+    const createRequest = {
+      restaurantId,
+      name: this.nameFromApi(name),
+      picture,
+      price,
+      category,
+    };
+
+    const restaurant = await this.restaurantMenuService.createRestaurantMenu(createRequest);
+
+    return this.getApiRestaurantMenu(restaurant);
+  }
+
+  @Get('/:menuId')
+  @Bind(Param())
+  async getRestaurantMenu(params) {
+    const { restaurantId, menuId } = params;
+
+    const query = {
+      _id: menuId,
+      restaurantId,
+    };
+    const restaurant = await this.restaurantMenuService.getRestaurantMenu(query);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant not found', HttpStatus.NOT_FOUND);
+    }
+
+    return this.getApiRestaurantMenu(restaurant);
+  }
+
+  @Put('/:menuId')
+  @Bind(Body(), Param())
+  async updateRestaurantMenu(body, params) {
+    const { restaurantId, menuId } = params;
+    const {
+      name, picture, price, category,
+    } = body;
+
+    const update = {};
+
+    if (name) {
+      update.name = this.nameFromApi(name);
+    }
+    if (picture) {
+      update.picture = picture;
+    }
+    if (price) {
+      update.price = price;
+    }
+    if (category) {
+      update.category = category;
+    }
+    update.updatedAt = Math.floor(Date.now() / 1000);
+
+    const query = {
+      _id: menuId,
+      restaurantId,
+    };
+
+    let restaurant = await this.restaurantMenuService.getRestaurantMenu(query);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant menu not found', HttpStatus.NOT_FOUND);
+    }
+
+    restaurant = await this.restaurantMenuService.updateRestaurantMenu(query, update);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant menu not updated', HttpStatus.NOT_FOUND);
+    }
+
+    return this.getApiRestaurantMenu(restaurant);
+  }
+
+  @Delete('/:menuId')
+  @Bind(Param())
+  @HttpCode(204)
+  async deleteRestaurantMenu(params) {
+    const { restaurantId, menuId } = params;
+
+    const query = {
+      _id: menuId,
+      restaurantId,
+    };
+    await this.restaurantMenuService.deleteRestaurantMenu(query);
+  }
+
+  /** **********************************
+   ********** UTILS functions **********
+   *********************************** */
+
+  nameFromApi(name) {
+    const trimmed = (name || '').trim();
+    if (!trimmed.length) {
+      throw new HttpException('Invalid value for `name`', HttpStatus.BAD_REQUEST);
+    }
+    return trimmed;
+  }
+
+  getApiRestaurantMenu(restaurantMenu) {
+    const {
+      _id,
+      restaurantId,
+      name,
+      picture,
+      price,
+      category,
+      createdAt,
+      updatedAt,
+    } = restaurantMenu;
+
+    return {
+      _id,
+      restaurantId,
+      name,
+      picture,
+      price,
+      category,
+      createdAt: new Date(createdAt * 1000),
+      updatedAt: new Date(updatedAt * 1000),
+    };
   }
 }
--- src/controllers/restaurant.controller.js
+++ src/controllers/restaurant.controller.js
@@ -1,15 +1,173 @@
 import {
-  Controller,
-  Logger,
+  Controller, Dependencies, Bind, Get, Post, Put, Delete,
+  Query, Body, Param, HttpCode, HttpException, HttpStatus, Logger,
 } from '@nestjs/common';
+import RestaurantService from '../services/restaurant.service';
 
 @Controller({
   path: 'restaurants',
   version: ['0.1'],
 })
 
+@Dependencies(RestaurantService)
 export default class RestaurantController {
-  constructor() {
+  constructor(restaurantService) {
+    this.restaurantService = restaurantService;
+
     this.logger = new Logger('restaurant.controller');
+  }
+
+  @Get('/')
+  @Bind(Query())
+  async listRestaurants(queryParams) {
+    const {
+      sortBy = 'createdAt',
+      order = 'asc',
+    } = queryParams;
+    let { page, perPage } = queryParams;
+
+    page = page ? parseInt(page, 10) : 1;
+    perPage = perPage ? parseInt(perPage, 10) : 10;
+
+    const ordering = order === 'asc' ? 1 : -1;
+
+    const query = {};
+    const skip = perPage * (page - 1);
+    const limit = perPage;
+    const sort = { [sortBy]: ordering };
+
+    const [totalCount, restaurants] = await Promise.all([
+      this.restaurantService.countRestaurants(query),
+      this.restaurantService.listRestaurants(query, skip, limit, sort),
+    ]);
+
+    const totalPages = parseInt(Math.ceil(totalCount / perPage), 10);
+    if (totalCount <= perPage) {
+      page = 1;
+    } else if (page > totalPages) {
+      page = totalPages;
+    }
+
+    return {
+      restaurants: restaurants.map((restaurant) => this.getApiRestaurant(restaurant)),
+      page,
+      perPage,
+      totalCount,
+    };
+  }
+
+  @Post('/')
+  @Bind(Body())
+  async createRestaurant(body) {
+    const { name, picture, address } = body;
+
+    const createRequest = {
+      name: this.nameFromApi(name),
+      picture,
+      address,
+    };
+
+    const restaurant = await this.restaurantService.createRestaurant(createRequest);
+
+    return this.getApiRestaurant(restaurant);
+  }
+
+  @Get('/:restaurantId')
+  @Bind(Param())
+  async getRestaurant(params) {
+    const { restaurantId } = params;
+
+    const query = {
+      _id: restaurantId,
+    };
+    const restaurant = await this.restaurantService.getRestaurant(query);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant not found', HttpStatus.NOT_FOUND);
+    }
+
+    return this.getApiRestaurant(restaurant);
+  }
+
+  @Put('/:restaurantId')
+  @Bind(Body(), Param())
+  async updateRestaurant(body, params) {
+    const { restaurantId } = params;
+    const { name, picture, address } = body;
+
+    const update = {};
+
+    if (name) {
+      update.name = this.nameFromApi(name);
+    }
+    if (picture) {
+      update.picture = picture;
+    }
+    if (address) {
+      update.address = address;
+    }
+    update.updatedAt = Math.floor(Date.now() / 1000);
+
+    const query = {
+      _id: restaurantId,
+    };
+
+    let restaurant = await this.restaurantService.getRestaurant(query);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant not found', HttpStatus.NOT_FOUND);
+    }
+
+    restaurant = await this.restaurantService.updateRestaurant(query, update);
+
+    if (!restaurant) {
+      throw new HttpException('Restaurant not updated', HttpStatus.NOT_FOUND);
+    }
+
+    return this.getApiRestaurant(restaurant);
+  }
+
+  @Delete('/:restaurantId')
+  @Bind(Param())
+  @HttpCode(204)
+  async deleteRestaurant(params) {
+    const { restaurantId } = params;
+
+    const query = {
+      _id: restaurantId,
+    };
+    await this.restaurantService.deleteRestaurant(query);
+  }
+
+  /** **********************************
+   ********** UTILS functions **********
+   *********************************** */
+
+  nameFromApi(name) {
+    const trimmed = (name || '').trim();
+    if (!trimmed.length) {
+      throw new HttpException('Invalid value for `name`', HttpStatus.BAD_REQUEST);
+    }
+    return trimmed;
+  }
+
+  getApiRestaurant(restaurant) {
+    const {
+      _id,
+      name,
+      picture,
+      address,
+      createdAt,
+      updatedAt,
+    } = restaurant;
+
+    return {
+      _id,
+      name,
+      picture,
+      address,
+      createdAt: new Date(createdAt * 1000),
+      updatedAt: new Date(updatedAt * 1000),
+    };
   }
 }
